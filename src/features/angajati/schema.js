9bc98159629b52ac2ad752e6e2f12717940e6088
Daniela Prisacariu <daniela.prisacariu@totalsoft.ro>
2022-09-23 14:21:42 +0300

    dp

---
+++ src/features/angajati/schema.js
@@ -0,0 +1,39 @@
+const { gql } = require('apollo-server-koa')
+
+const angajatDefs = gql`
+  type Angajat {
+    id: Int!
+    nume: String!
+    prenume: String!
+    email: String!
+    parola: String!
+    dataAngajare: DateTime!
+    dataNasterii: DateTime!
+    cnp: String!
+    serie: String!
+    no: String!
+    nrTelefon: String
+    #poza: Byte[]
+    esteAdmin: Boolean
+    # Manager: Angajat
+    departament: Departament
+    functie: Functie
+    concediat: Boolean
+  }
+
+  type Departament {
+    id: Int!
+    denumire: String
+  }
+
+  type Functie {
+    id: Int!
+    denumire: String
+  }
+
+  extend type Query {
+    angajat{id: Int}: Angajat!
+  }
+`
+
+module.exports = angajatDefs
--- src/startup/dataSources.js
+++ src/startup/dataSources.js
@@ -1,15 +1,19 @@
 const UserApi = require('../features/user/dataSources/userApi')
 const UserDb = require('../features/user/dataSources/userDb')
 
+const AngajatApi = require('../features/angajati/dataSources/angajatiAPI')
+
 module.exports.getDataSources = () => ({
   // Instantiate your data sources here. e.g.: userApi: new UserApi()
   userApi: new UserApi(),
-  userDb: new UserDb()
+  userDb: new UserDb(),
+  AngajatApi: new AngajatApi()
 })
 
 module.exports.initializedDataSources = (context, dbInstance, dataSources) => {
   // You need to initialize you datasources here e.g.: dataSources.userApi.initialize({ context })
   dataSources.userApi.initialize({ context })
   dataSources.userDb.initialize({ context: { dbInstance } })
+  dataSources.AngajatApi.initialize({ context })
   return dataSources
 }
