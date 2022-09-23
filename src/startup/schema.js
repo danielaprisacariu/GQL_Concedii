2747c8002e087b376435a4c93515d22c27d62143
Daniela Prisacariu <daniela.prisacariu@totalsoft.ro>
2022-09-23 14:21:10 +0300

    dp

--- src/startup/schema.js
+++ src/startup/schema.js
@@ -8,14 +8,18 @@ const { join } = require('path')
 const userResolvers = require('../features/user/resolvers')
 const userDefs = require('../features/user/schema')
 
+const angajatResolvers = require('../features/angajati/resolvers')
+const angajatDefs = require('../features/angajati/schema')
+
 const oldTypeDefs = []
 const sources = loadTypedefsSync(join(__dirname, '../**/*.graphql'), {
   loaders: [new GraphQLFileLoader()]
 })
 
-const resolvers = merge(userResolvers)
+const resolvers = merge(userResolvers, angajatResolvers)
+//const resolvers = merge(angajatResolvers)
 
-const typeDefs = [...sources.map(source => source.document), ...oldTypeDefs, userDefs]
+const typeDefs = [...sources.map(source => source.document), ...oldTypeDefs, userDefs, angajatDefs]
 
 module.exports = makeExecutableSchema({ typeDefs, resolvers })
 module.exports.tests = { typeDefs, resolvers }
