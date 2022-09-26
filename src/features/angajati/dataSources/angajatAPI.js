4bc84073cda4014f56ae9dfe504f222ee7a53111
Daniela Prisacariu <daniela.prisacariu@totalsoft.ro>
2022-09-26 14:10:02 +0300

    dp

--- src/features/angajati/dataSources/angajatAPI.js
+++ src/features/angajati/dataSources/angajatAPI.js
@@ -24,23 +24,3 @@ class AngajatApi extends ApiRESTDataSource {
 }
 
 module.exports = AngajatApi
-
-// {
-//   id: 1,
-//   nume: 'Mitrut',
-//   prenume: 'Elena',
-//   email: 'elena.mitrut@totalsoft.ro',
-//   parola: 'mitrutElena1',
-//   dataAngajare: new Date(),
-//   dataNasterii: new Date(),
-//   cnp: '2678909876543',
-//   serie: 'DP',
-//   no: '123456',
-//   nrTelefon: '0745678909876',
-//   //   poza: null,
-//   esteAdmin: false,
-//   //   manager: null,
-//   departament: null,
-//   functie: null,
-//   concediat: false
-// }
