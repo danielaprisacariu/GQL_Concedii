2d5bcaacfef26d5194a7e1895d5d1d8dc04ebe0e
Erwin Brustur <erwin.brustur@totalsoft.ro>
2022-09-28 17:49:23 +0300

    eb

--- src/features/concedii/resolvers.js
+++ src/features/concedii/resolvers.js
@@ -5,6 +5,21 @@ const concediuResolver = {
     inserareConcediu: async (_, { input }, { dataSources }) => {
       return dataSources.concediuApi.inserareConcediu(input)
     }
+  },
+  Query: {
+    zileRamase: async (_, { id, tipConcediuId }, { dataSources }) => {
+      const data = await dataSources.concediuApi.getZileRamase(id, tipConcediuId)
+      return data
+    },
+    AllIstoricConcediiVerificare: async (_, { id }, { dataSources }) => {
+      const data = await dataSources.concediuApi.getAllIstoricConcediiVerificare(id)
+      return data
+    },
+
+    ConcediiInlocuitori: async (_, { id }, { dataSources }) => {
+      const data = await dataSources.concediuApi.getConcediiInlocuitori(id)
+      return data
+    }
   }
 }
 
