85556f8ca3432fcdec7a87ac28dfec952bf1521e
Damian Mares <damian.mares@totalsoft.ro>
2022-09-28 09:47:14 +0300

    meow

--- src/features/angajati/resolvers.js
+++ src/features/angajati/resolvers.js
@@ -3,17 +3,17 @@ const { topics, pubSub } = require('../../pubSub')
 const angajatResolver = {
   Query: {
     angajat: async (_, { id }, { dataSources }) => {
-      const data = await dataSources.AngajatApi.getAngajat(id)
+      const data = await dataSources.angajatApi.getAngajat(id)
       return data
     },
     angajati: async (_, __, { dataSources }) => {
-      const data = await dataSources.AngajatApi.getAllAngajat()
+      const data = await dataSources.angajatApi.getAllAngajat()
       return data
     }
   },
   Mutation: {
     newAngajat: async (_, { input }, { dataSources }) => {
-      return dataSources.angajatAPI.newAngajat(input)
+      return dataSources.angajatApi.newAngajat(input)
     }
   }
 }
